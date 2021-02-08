d5c981502ed7bb0e8d1e590d5846b4b7d95d1e17
Kaan-Abi <kaan_34@outlook.fr>
2021-02-08 14:28:42 +0100

    ss

--- gamejam2021.py
+++ gamejam2021.py
@@ -1,68 +1,156 @@
 import pygame, sys
 from pygame.locals import *
-from Game import Game
+from Player import Player
+import random, time
+
+
+
+# Initializing
 pygame.init()
 
+# Setting up FPS
+FPS = 60
 FramePerSec = pygame.time.Clock()
 
-
-WHITE = (255, 255, 255)
+# Creating colors
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-DISPLAYSURF = pygame.display.set_mode((1024,768))
+# Other Variables for use in the program
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 600
+SPEED = 5
+SCORE = 0
+
+# Setting up Fonts
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, BLACK)
+
+# Create a white screen
+DISPLAYSURF = pygame.display.set_mode((1024, 768))
 DISPLAYSURF.fill(WHITE)
-pygame.display.set_caption("ForestCape")
+pygame.display.set_caption("Game")
 
-background = pygame.image.load('images/day_forest.png')
-background_size = background.get_size()
-background_rect = background.get_rect()
 
-w,h = background_size
-x = 0
-y = 0
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Enemy.png")
+        self.surf = pygame.Surface((42, 70))
+        self.rect = self.surf.get_rect(center=(random.randint(40, SCREEN_WIDTH - 40)
+                                               , 0))
 
-x1 = 0
-y1 = -h
+    def move(self):
+        global SCORE
+        self.rect.move_ip(0, SPEED)
+        if (self.rect.top > 600):
+            SCORE += 1
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
-running = True
 
-game = Game()
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("images/tree_player.png")
+        self.surf = pygame.Surface((40, 75))
+        self.rect = self.surf.get_rect(center=(160, 520))
 
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        # if pressed_keys[K_UP]:
+        # self.rect.move_ip(0, -5)
+        # if pressed_keys[K_DOWN]:
+        # self.rect.move_ip(0,5)
+
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+class Background():
+    def __init__(self):
+        self.bgimage = pygame.image.load('images/day_forest.png')
+        self.rectBGimg = self.bgimage.get_rect()
+
+        self.bgY1 = 0
+        self.bgX1 = 0
+
+        self.bgY2 = 0
+        self.bgX2 = self.rectBGimg.width
+
+        self.moving_speed = 5
+
+    def update(self):
+        self.bgX1 -= self.moving_speed
+        self.bgX2 -= self.moving_speed
+        if self.bgX1 <= -self.rectBGimg.width:
+            self.bgX1 = self.rectBGimg.width
+        if self.bgX2 <= -self.rectBGimg.width:
+            self.bgX2 = self.rectBGimg.width
+
+    def render(self):
+        DISPLAYSURF.blit(self.bgimage, (self.bgX1, self.bgY1))
+        DISPLAYSURF.blit(self.bgimage, (self.bgX2, self.bgY2))
+
+# Setting up Sprites
+P1 = Player()
+back_ground = Background()
+
+# Creating Sprites Groups
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+
+# Adding a new User event
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+
+# Game Loop
 while True:
 
-    #background spawing and moving
-    while running:
-        DISPLAYSURF.blit(background, background_rect)
-        pygame.display.update()
-        for event in pygame.event.get(): 
-            if event.type == QUIT:
-                running = False
-                pygame.quit()
-                sys.exit()
-        y1 += 5
-        y += 5
-        DISPLAYSURF.blit(background,(-y,-x))
-        DISPLAYSURF.blit(background,(-y1,-x1))
-        #player spawning
-        DISPLAYSURF.blit(game.player.image, game.player.rect)
-        if y > h:
-            y = -h
-        if y1 > h:
-            y1 = -h
-        pygame.display.flip()
-        pygame.display.update()
-        FramePerSec.tick(15)
-                
-    #pygame.display.update()
-
-    for event in pygame.event.get(): 
+    # Cycles through all occurring events
+    for event in pygame.event.get():
+        if event.type == INC_SPEED:
+            SPEED += 0.5
         if event.type == QUIT:
-            running = false
             pygame.quit()
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
-                #which button is used ? 
-            if event.key == pygame.K_SPACE:
-                break
+
+    back_ground.update()
+    back_ground.render()
+
+    # DISPLAYSURF.blit(background, (0,0))
+    scores = font_small.render(str(SCORE), True, BLACK)
+    DISPLAYSURF.blit(scores, (10, 10))
+
+    # Moves and Re-draws all Sprites
+    for entity in all_sprites:
+        DISPLAYSURF.blit(entity.image, entity.rect)
+        entity.move()
+
+    # To be run if collision occurs between Player and Enemy
+    if pygame.sprite.spritecollideany(P1, enemies):
+        pygame.mixer.Sound('crash.wav').play()
+        time.sleep(0.8)
+
+        DISPLAYSURF.fill(RED)
+        DISPLAYSURF.blit(game_over, (30, 250))
+
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill()
+        time.sleep(1.5)
+        pygame.quit()
+        sys.exit()
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
 
                 
