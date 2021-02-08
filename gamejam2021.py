66e9f7f04bf8c54a94d34be84025a59db06d8f7d
Ayache Inzouddine <ayache.inzouddine@etu.univ-grenoble-alpes.fr>
2021-02-08 10:22:21 +0100

    Co-authored-by: Zinedine BOUARICHE <zinede13@hotmail.fr>

--- gamejam2021.py
+++ gamejam2021.py
@@ -11,12 +11,17 @@ BLACK = (0, 0, 0)
 
 DISPLAYSURF = pygame.display.set_mode((1024,768))
 DISPLAYSURF.fill(WHITE)
-pygame.display.set_caption("Game")
+pygame.display.set_caption("ForestCape")
+background = pygame.image.load('images/game.png')
 
-pygame.draw.line(DISPLAYSURF, BLACK, (150,130), (130,170))
-pygame.draw.line(DISPLAYSURF, BLACK, (150,130), (170,170))
+#pygame.draw.line(DISPLAYSURF, BLACK, (150,130), (130,170))
+#pygame.draw.line(DISPLAYSURF, BLACK, (150,130), (170,170))
 
 while True:
+
+    #background applying
+    DISPLAYSURF.blit(background, (-500,-200))
+
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == QUIT:
