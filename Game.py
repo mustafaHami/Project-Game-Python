42d175ad98b25c83a6fbb370bb7ad03a794dbf19
Ayache Inzouddine <ayache.inzouddine@etu.univ-grenoble-alpes.fr>
2021-02-09 12:42:35 +0100

    yes

--- Game.py
+++ Game.py
@@ -1,8 +1,25 @@
 import pygame
 from Player import Player
+from Fruits import Fruits
 
 class Game:
 
     def __init__(self):
+        #player status
+        #self.is_playing = False
         # generate a player 
-        self.player = Player()
+        self.all_players = pygame.sprite.Group()
+        self.player = Player(self)
+        self.all_players.add(self.player)
+        #generate fruits
+        self.all_fruits = pygame.sprite.Group()
+        self.pressed = {}
+        for i in range(3):
+            self.spawn_fruit()
+    
+    def check_collision(self, sprite, group):
+        return pygame.sprite.spritecollide(sprite, group, False, pygame.sprite.collide_mask)
+
+    def spawn_fruit(self):
+        fruit = Fruits(self)
+        self.all_fruits.add(fruit)
--- Player.py
+++ Player.py
@@ -1,32 +1,10 @@
 from pygame.locals import *
 import time
 import pygame
-# Setting up FPS
-FPS = 60
-FramePerSec = pygame.time.Clock()
-
-# Creating colors
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-
-# Other Variables for use in the program
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 600
-SPEED = 5
-SCORE = 0
-
-# Create a white screen
-DISPLAYSURF = pygame.display.set_mode((400, 600))
-DISPLAYSURF.fill(WHITE)
-pygame.display.set_caption("Game")
-
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, game):
         super().__init__()
         self.health = 100
         self.max_health = 100
@@ -39,7 +17,10 @@ class Player(pygame.sprite.Sprite):
         self.rect.y = 550
 
     def move_right(self):
-        self.rect.x += self.velocity
+        if not self.game.check_collision(self, self.game.all_fruits):
+            self.rect.x += self.velocity
+        for fruit in self.player.game.check_collision(self, self.player.game.all_fruits):
+            fruit.damage(10)
 
 
     def move(self):
@@ -48,7 +29,6 @@ class Player(pygame.sprite.Sprite):
         # self.rect.move_ip(0, -5)
         # if pressed_keys[K_DOWN]:
         # self.rect.move_ip(0,5)
-
         if self.rect.left > 0:
             if pressed_keys[K_LEFT]:
                 self.rect.move_ip(-5, 0)
