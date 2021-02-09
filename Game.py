27c797d40a713a0815d4a145d60302f7c094b0a1
zinedine <zinede13@hotmail.fr>
2021-02-09 16:46:12 +0100

    a

--- Game.py
+++ Game.py
@@ -2,6 +2,31 @@ import pygame
 from Player import Player
 from Fruits import Fruits
 
+class Background():
+    def __init__(self):
+        self.bgimage = pygame.image.load('images/background.jpg')
+        self.bgimage = pygame.transform.scale(self.bgimage, (1024, 768))
+        self.rectBGimg = self.bgimage.get_rect()
+
+        self.bgY1 = 0
+        self.bgX1 = 0
+
+        self.bgY2 = 0
+        self.bgX2 = self.rectBGimg.width
+
+        self.moving_speed = 5           
+
+    def update(self):
+        self.bgX1 -= self.moving_speed
+        self.bgX2 -= self.moving_speed
+        if self.bgX1 <= -self.rectBGimg.width:
+            self.bgX1 = self.rectBGimg.width
+        if self.bgX2 <= -self.rectBGimg.width:
+            self.bgX2 = self.rectBGimg.width
+
+    def render(self):
+        DISPLAYSURF.blit(self.bgimage, (self.bgX1, self.bgY1))
+        DISPLAYSURF.blit(self.bgimage, (self.bgX2, self.bgY2))
 
 class Game:
 
--- gamejam2021.py
+++ gamejam2021.py
@@ -1,6 +1,7 @@
 import pygame, sys
 from pygame.locals import *
 from Game import Game
+from Game import Background
 import time
 # Initializing
 pygame.init()
@@ -35,31 +36,6 @@ pygame.display.set_caption("Forescape")
 
 
 
-class Background():
-    def __init__(self):
-        self.bgimage = pygame.image.load('images/background.jpg')
-        self.bgimage = pygame.transform.scale(self.bgimage, (1024, 768))
-        self.rectBGimg = self.bgimage.get_rect()
-
-        self.bgY1 = 0
-        self.bgX1 = 0
-
-        self.bgY2 = 0
-        self.bgX2 = self.rectBGimg.width
-
-        self.moving_speed = 5           
-
-    def update(self):
-        self.bgX1 -= self.moving_speed
-        self.bgX2 -= self.moving_speed
-        if self.bgX1 <= -self.rectBGimg.width:
-            self.bgX1 = self.rectBGimg.width
-        if self.bgX2 <= -self.rectBGimg.width:
-            self.bgX2 = self.rectBGimg.width
-
-    def render(self):
-        DISPLAYSURF.blit(self.bgimage, (self.bgX1, self.bgY1))
-        DISPLAYSURF.blit(self.bgimage, (self.bgX2, self.bgY2))
 
 
 # Setting up Sprites
@@ -127,7 +103,7 @@ while True:
 
     #Health Bar
     game.player.update_health_bar(DISPLAYSURF)
-    
+
 
     for fruits in game.all_fruits:
         fruits.forward()
