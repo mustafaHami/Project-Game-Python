bf97714f2e28f2247aa7f87e6cd02073f57d6d6f
Ayache Inzouddine <ayache.inzouddine@etu.univ-grenoble-alpes.fr>
2021-02-10 11:35:57 +0100

    yy

--- Game.py
+++ Game.py
@@ -1,33 +1,6 @@
 import pygame, sys
 from Player import Player
 from Fruits import Fruits
-DISPLAYSURF = pygame.display.set_mode((1024, 768))
-class Background():
-    def __init__(self):
-        self.bgimage = pygame.image.load('images/background.jpg')
-        self.bgimage = pygame.transform.scale(self.bgimage, (1024, 768))
-        self.rectBGimg = self.bgimage.get_rect()
-
-        self.bgY1 = 0
-        self.bgX1 = 0
-
-        self.bgY2 = 0
-        self.bgX2 = self.rectBGimg.width
-
-        self.moving_speed = 5           
-
-    def update(self):
-        self.bgX1 -= self.moving_speed
-        self.bgX2 -= self.moving_speed
-        if self.bgX1 <= -self.rectBGimg.width:
-            self.bgX1 = self.rectBGimg.width
-        if self.bgX2 <= -self.rectBGimg.width:
-            self.bgX2 = self.rectBGimg.width
-
-    def render(self):
-        DISPLAYSURF.blit(self.bgimage, (self.bgX1, self.bgY1))
-        DISPLAYSURF.blit(self.bgimage, (self.bgX2, self.bgY2))
-
 
 class Game:
 
@@ -42,7 +15,6 @@ class Game:
         self.all_players.add(self.player)
         self.score = 0
         self.highest_score = [0]
-        #self.double_jump = False
         #generate fruits
         self.all_fruits = pygame.sprite.Group()
         self.pressed = {}
--- manu.py
+++ manu.py
@@ -1,58 +1,69 @@
 import pygame, sys
 from pygame.locals import *
 from Game import Game
-from Game import Background
 import time
+import random
 from tkinter import * 
-
 # Initializing
 pygame.init()
-#///////////////////////////////////////DISPLAY PARAMETERS////////////////////////////////////////////////////////
+
+# Setting up FPS
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
 # Creating colors
+BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-# Adding a new User event
-INC_SPEED = pygame.USEREVENT + 1
-pygame.time.set_timer(INC_SPEED, 1000)
+# Other Variables for use in the program
+SPEED = 5
 
 # Setting up fonts that will be used
 font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
-game_over = font.render("Game Over", True, (0,0,0))
+game_over = font.render("Game Over", True, BLACK)
 
 # Create a white screen
 DISPLAYSURF = pygame.display.set_mode((1024, 768))
-#DISPLAYSURF.fill(WHITE)
-
-#import banner 
-banner = pygame.image.load('images/night_forest.png')
-banner = pygame.transform.scale(banner, (1024, 768))
-
-#import logo forescape
-logo = pygame.image.load('images/logo_size.png')
-logo = pygame.transform.scale(logo, (500, 100))
-
-#import button 
-play_button = pygame.image.load('images/power.png')
-play_button = pygame.transform.scale(play_button,(70,70))
-play_button_rect = play_button.get_rect()
-play_button_rect.x = 700
-play_button_rect.y = 250
-
-
-DISPLAYSURF.blit(banner,(0,0))
-DISPLAYSURF.blit(logo,(100,100))
-
+DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Forescape")
-#///////////////////////////////////////DISPLAY PARAMETERS////////////////////////////////////////////////////////
-game = Game()
 
+class Background():
+    def __init__(self):
+        self.bgimage = pygame.image.load('images/background.jpg')
+        self.bgimage = pygame.transform.scale(self.bgimage, (1024, 768))
+        self.rectBGimg = self.bgimage.get_rect()
+
+        self.bgY1 = 0
+        self.bgX1 = 0
+
+        self.bgY2 = 0
+        self.bgX2 = self.rectBGimg.width
+
+        self.moving_speed = 5           
+
+    def update(self):
+        self.bgX1 -= self.moving_speed
+        self.bgX2 -= self.moving_speed
+        if self.bgX1 <= -self.rectBGimg.width:
+            self.bgX1 = self.rectBGimg.width
+        if self.bgX2 <= -self.rectBGimg.width:
+            self.bgX2 = self.rectBGimg.width
+
+    def render(self):
+        DISPLAYSURF.blit(self.bgimage, (self.bgX1, self.bgY1))
+        DISPLAYSURF.blit(self.bgimage, (self.bgX2, self.bgY2))
+
+# Setting up Sprites
+game = Game()
+P1 = game.player
+back_ground = Background()
 
 def rungame():
     if len(name_entry.get()) != 0:
-        print(name_entry.get())
         window.destroy()
         game.is_playing = True
 
@@ -79,7 +90,7 @@ label_title = Label(frame,text="Enter your name",font=("Courrier",40), bg = "#97
 label_title.grid(row=1,column=0,sticky=W)
 name_entry = Entry(frame,font=("Courrier",40), bg = "#97CE68",fg="black")
 name_entry.grid(row=1,column=1,sticky=W)
-
+name = name_entry.get()
 frame.pack(expand=YES)
 #addign a button 
 play_button = Button(window,text="Play",font=("Courrier",40), bg = "white",fg="green",command=rungame)
@@ -88,83 +99,78 @@ play_button.pack(fill=X)
 window.mainloop()
 #/////////////////////////////MENU WINDOW//////////////////////////////////////////////////////////
 
-# Setting up Sprites
-P1 = game.player
-back_ground = Background() 
 
-saut = 20
-jump = False
-# Game Loop
+
 
 # Creating Sprites Groups
 enemies = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 
-# Other Variables for use in the program
-SPEED = 5
-SCORE = 0
+# Adding a new User event
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+saut = 20
+jump = False
+random = random.randint(0,100)
 
-#/////////////////////////////////////THE GAME IS RUNNING///////////////////////////////////////////////////////
-# Every game events
+# Game Loop
 while True:
 
-    if game.is_playing == True:
-        for event in pygame.event.get():
-            if event.type == INC_SPEED:
-                SPEED += 0.5
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE or event.key == pygame.K_UP and saut == 20:
-                    jump = True
-
-        if jump == True and saut >= -20:
-            saut -= 1
-            P1.jumpy(saut)
-
-        if saut == -19:
-            saut = 20
-            jump = False
-
-        back_ground.update()
-        back_ground.render()
-
-        # screen.blit(background, (0,0))
-        scores = font_small.render(str(SCORE), True, BLACK)
-        DISPLAYSURF.blit(scores, (10, 10))
-        # Add fruits
-        game.all_fruits.draw(DISPLAYSURF)
-        # Moves and Re-draws
-        for entity in all_sprites:
-            DISPLAYSURF.blit(entity.image, entity.rect)
-            entity.move()
-
-        for fruits in game.all_fruits:
-            fruits.forward()
-
-        # To be run if collision occurs between Player and Enemy
-        if pygame.sprite.spritecollideany(P1, enemies):
-            pygame.mixer.Sound('crash.wav').play()
-            time.sleep(0.8)
-
-            screen.fill(RED)
-            screen.blit(game_over, (30, 250))
-
-            pygame.display.update()
-            for entity in all_sprites:
-                entity.kill()
-            time.sleep(1.5)
+    # Every game events
+    for event in pygame.event.get():
+        if event.type == INC_SPEED:
+            SPEED += 0.5
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE or event.key == pygame.K_UP or event.key == pygame.K_z and saut == 20:
+                jump = True
+    if jump == True and saut >= -20:
+        saut -= 1
+        P1.jumpy(saut)
 
+    if saut == -19:
+        saut = 20
+        jump = False
 
-            pygame.display.update()
-            pygame.time.Clock().tick(60)
-        #if the game didn't start
+    back_ground.update()
+    back_ground.render()
+
+    # Add fruits
+    game.all_fruits.draw(DISPLAYSURF)
+    # Moves and Re-draws
+
+    for entity in all_sprites:
+        DISPLAYSURF.blit(entity.image, entity.rect)
+        entity.move()
+
+    #Health Bar
+    game.player.update_health_bar(DISPLAYSURF)
+
+    #Animation
+    game.player.update_animation()
+
+    for fruits in game.all_fruits:
+        fruits.forward()
+        game.update(DISPLAYSURF)
+        pygame.display.update()
+
+    # To be run if collision occurs between Player and Enemy
+    if pygame.sprite.spritecollideany(P1, enemies):
+        time.sleep(0.8)
+
+        DISPLAYSURF.fill(RED)
+        DISPLAYSURF.blit(game_over, (30, 250))
+
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill()
+        time.sleep(1.5)
+        pygame.quit()
+        sys.exit()
 
 
     pygame.display.update()
-    
-#/////////////////////////////////////THE GAME IS RUNNING///////////////////////////////////////////////////////
+    FramePerSec.tick(FPS)
