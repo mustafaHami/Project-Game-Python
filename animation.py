4d9c415f51ae78392a62f8d5f320f8f9ed80f3f5
zinedine <zinede13@hotmail.fr>
2021-02-10 16:55:54 +0100

    a

--- animation.py
+++ animation.py
@@ -7,15 +7,24 @@ class AnimateSprite(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(pygame.image.load(f'animation/{sprite_name}.png'),(75,125))
         self.current_image = 0
         self.images = animations.get(sprite_name)
+        self.animation = False
+    
+    def start_animation(self):
+        self.animation = True
+    
+    def stop_animation(self):
+        self.animation = False
 
     def animate(self):
 
-        self.current_image += 1
+        if self.animation:
 
-        if self.current_image >= len(self.images):
-            self.current_image = 0
+            self.current_image += 1
 
-        self.image = self.images[self.current_image]       
+            if self.current_image >= len(self.images):
+                self.current_image = 0
+
+            self.image = self.images[self.current_image]       
 
 
 
@@ -23,7 +32,7 @@ def load_animation_images(sprite_name):
 
     images = []
 
-    path = f"animation/{sprite_name}/{sprite_name}"
+    path = f'animation/{sprite_name}/{sprite_name}'
 
     for num in range(1,28):
         image_path = path + str(num) + '.png'
