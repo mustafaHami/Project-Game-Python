86deb2d192c7e50039826a1c1b7b892c7309360b
Ayache Inzouddine <ayache.inzouddine@etu.univ-grenoble-alpes.fr>
2021-02-10 10:51:05 +0100

    yes

--- Fruits.py
+++ Fruits.py
@@ -1,8 +1,8 @@
 import pygame
 import random
 
-eat = ["strawberry", "kiwi","orange","banana","coconut","grape","green_apple"]
-eat2 = ["strawberry", "kiwi","orange","banana","coconut","grape","green_apple", "chainsaw"]
+eat = ["strawberry", "kiwi","orange","banana","coconut","grape","green_apple", "Cabbage", "Carrot", "Broccoli", "Cucumber", "Pumpkin", "Tomato"]
+eat2 = ["strawberry", "kiwi","orange","banana","coconut","grape","green_apple", "Cabbage", "Carrot", "Broccoli", "Cucumber", "Pumpkin", "Tomato", "cursed_apple", "cursed_strawberry", "cursed_banana"]
 
 class Fruits(pygame.sprite.Sprite):
 
@@ -10,7 +10,7 @@ class Fruits(pygame.sprite.Sprite):
         super().__init__()
         self.game = game
         self.damage = 30
-        self.bonus = 1
+        self.bonus = 2
         self.image_name = "kiwi"
         self.image = pygame.image.load("images/"+random.choice(eat)+".png")
         self.rect = self.image.get_rect()
@@ -29,7 +29,7 @@ class Fruits(pygame.sprite.Sprite):
         return self.score
     
     def forward(self):
-        if self.image_name != "chainsaw":
+        if self.image_name != "cursed_apple" and self.image_name != "cursed_strawberry" and self.image_name != "cursed_banana":
             #if the player does not collide a fruit
             if not self.game.check_collision(self, self.game.all_players):
                 self.rect.x -= self.velocity
@@ -49,6 +49,7 @@ class Fruits(pygame.sprite.Sprite):
                 if(self.game.player.health <= 97):
                     self.game.player.bonus(self.bonus)
                 self.rect.x = 1300 + random.randint(0,100)
+                print(self.image_name)
                 self.image = pygame.image.load("images/"+self.image_name+".png")
 
                 position = random.randint(1,2)
@@ -60,11 +61,9 @@ class Fruits(pygame.sprite.Sprite):
                     self.rect.y = 550
                     self.image = pygame.image.load("images/"+self.image_name+".png")
                 position = random.randint(1,2)
-                
-   
+            
 
-
-        elif self.image_name == "chainsaw":
+        elif self.image_name == "cursed_apple" or self.image_name == "cursed_strawberry" or self.image_name == "cursed_banana":
             #if the player does not collide a fruit
             if not self.game.check_collision(self, self.game.all_players):
                 self.rect.x -= self.velocity
@@ -76,8 +75,20 @@ class Fruits(pygame.sprite.Sprite):
                 self.velocity += 0.2
                 if self.velocity > 25:
                     self.velocity = 25
+
                 self.image_name = random.choice(eat2)
                 self.game.player.damage(self.damage)
+
+                if self.game.player.health <= 0:
+                    print("Game Over !")
+                    self.game.highScore(self.game.score)
+
+                if self.game.score >= 20:
+                    self.game.score = self.game.score - 20
+                else:
+                    self.game.player.score = 0
+                    self.score = 0
+                
                 self.rect.x = 1300 + random.randint(0,100)
                 self.image = pygame.image.load("images/"+self.image_name+".png")
 
--- Game.py
+++ Game.py
@@ -12,20 +12,31 @@ class Game:
         self.player = Player(self)
         self.all_players.add(self.player)
         self.score = 0
+        self.highest_score = [0]
+        #self.double_jump = False
         #generate fruits
         self.all_fruits = pygame.sprite.Group()
         self.pressed = {}
-        for i in range(3):
+        for i in range(5):
             self.spawn_fruit()
     
     def check_collision(self, sprite, group):
         return pygame.sprite.spritecollide(sprite, group, False, pygame.sprite.collide_mask)
     
     def update(self, screen):
-        Police = pygame.font.Font("FONTS/bold_game_font_7.ttf", 40)
+        Police = pygame.font.Font("Fonts/bold_game_font_7.ttf", 40)
         Rendu = Police.render(f"Score : {self.score}", 1, (255,255,255)) 
         screen.blit(Rendu, (10, 40))
 
     def spawn_fruit(self):
         fruit = Fruits(self)
         self.all_fruits.add(fruit)
+
+    def highScore(self, score):
+        for i in self.highest_score:
+            if score > i:
+                self.highest_score.append(score)
+                print(self.highest_score[1])
+                break
+            print(self.highest_score[1])
+       
