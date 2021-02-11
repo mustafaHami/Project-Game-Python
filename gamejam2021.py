e005b98ad4fa2008e36c4aeadcc8ea1cf7b0357c
Ayache Inzouddine <ayache.inzouddine@etu.univ-grenoble-alpes.fr>
2021-02-11 10:31:28 +0100

    y

--- gamejam2021.py
+++ gamejam2021.py
@@ -72,22 +72,23 @@ def rungame():
 
 def tutorial():
     tuto_window = Toplevel()
+    tuto_window.title("Tutorial")
     tuto_window.geometry("1024x768")
-    tuto_window.config(background ="#97CE68")
+    tuto_window.config(background ="#008000")
 
-    inst = Label(tuto_window,text="Escape the forest !",font=("Courrier",50), bg = "#97CE68",fg="black")
+    inst = Label(tuto_window,text="Escape the forest !",font=("Courrier",50), bg = "#008000",fg="black")
     inst.grid(row=0,column=0,sticky=W) 
-    inst2 = Label(tuto_window,text="Avoid the cursed purple fruits they reduce your life bar",font=("Courrier",15), bg = "#97CE68",fg="black")
+    inst2 = Label(tuto_window,text="Avoid the cursed purple fruits, they reduce your sleep bar !",font=("Courrier",15), bg = "#008000",fg="black")
     inst2.grid(row=1,column=0,sticky=W) 
-    inst3 = Label(tuto_window,text="Collect the normal ones to increase your score and your life",font=("Courrier",15), bg = "#97CE68",fg="black")
+    inst3 = Label(tuto_window,text="Collect the normal fruits to increase your score and your life !",font=("Courrier",15), bg = "#008000",fg="black")
     inst3.grid(row=2,column=0,sticky=W) 
-    inst4 = Label(tuto_window,text="If your life bar is empty you fall asleep",font=("Courrier",15), bg = "#97CE68",fg="black")
+    inst4 = Label(tuto_window,text="If your sleep bar is empty, you will fall asleep",font=("Courrier",15), bg = "#008000",fg="black")
     inst4.grid(row=3,column=0,sticky=W) 
-    inst5 = Label(tuto_window,text="You need to face the monsters of your nightmare to wake up and continue your escape",font=("Courrier",15), bg = "#97CE68",fg="black")
+    inst5 = Label(tuto_window,text="You need to face the monsters of your nightmare to wake up and continue your escape",font=("Courrier",15), bg = "#008000",fg="black")
     inst5.grid(row=4,column=0,sticky=W)
     
     
-    inst6 = Label(tuto_window,text="If you fail you die",font=("Courrier",15), bg = "#97CE68",fg="black")
+    inst6 = Label(tuto_window,text="If you fail you die",font=("Courrier",15), bg = "#008000",fg="black")
     inst6.grid(row=5,column=0,sticky=W) 
 
     widthe = 100
@@ -107,17 +108,52 @@ def tutorial():
     
     tuto_window.mainloop()
 
+def credits():
+    credit_window = Toplevel()
+    credit_window.title("Credits")
+    credit_window.geometry("1024x768")
+    credit_window.config(background ="black")
+
+
+    
+    insta = Label(credit_window,text="Credits",font=("Courrier",50), bg = "black",fg="white")
+    insta.grid(row=0,column=1,sticky=W) 
+    inst = Label(credit_window,text="Gameplay programmers :",font=("Courrier",30), bg = "black",fg="#008000")
+    inst.grid(row=1,column=0,sticky=W) 
+    inst2 = Label(credit_window,text="- Ayache INZOUDDINE",font=("Courrier",15), bg = "black",fg="white")
+    inst2.grid(row=2,column=0,sticky=W) 
+    inst3 = Label(credit_window,text="- Kaan MARAZ",font=("Courrier",15), bg = "black",fg="white")
+    inst3.grid(row=3,column=0,sticky=W) 
+    inst4 = Label(credit_window,text="- Zinedine BOUARICHE",font=("Courrier",15), bg = "black",fg="white")
+    inst4.grid(row=4,column=0,sticky=W) 
+    inst5 = Label(credit_window,text="- Mustafa DEMIR\n",font=("Courrier",15), bg = "black",fg="white")
+    inst5.grid(row=5,column=0,sticky=W)
+
+    inst6 = Label(credit_window,text="Original soundtrack :",font=("Courrier",30), bg = "black",fg="#008000")
+    inst6.grid(row=6,column=0,sticky=W) 
+    inst7 = Label(credit_window,text="- Mustafa DEMIR\n",font=("Courrier",15), bg = "black",fg="white")
+    inst7.grid(row=7,column=0,sticky=W)
+
+    inst8 = Label(credit_window,text="Ressources :",font=("Courrier",30), bg = "black",fg="#008000")
+    inst8.grid(row=8,column=0,sticky=W)
+    inst9 = Label(credit_window,text="https://www.gamedevmarket.net/category/2d/",font=("Courrier",15), bg = "black",fg="white")
+    inst9.grid(row=9,column=0,sticky=W)
+    inst10 = Label(credit_window,text="https://www.pinterest.fr/",font=("Courrier",15), bg = "black",fg="white")
+    inst10.grid(row=10,column=0,sticky=W)
+
+    credit_window.mainloop()
+
 
 #/////////////////////////////MENU WINDOW//////////////////////////////////////////////////////////
 window = Tk()
 
 #Menu 
-window.title("Forescape Main Menu")
+window.title("Main Menu")
 window.geometry("1024x768")
-window.config(background ="#97CE68")
+window.config(background ="#008000")
 
 #creating Frame 
-frame = Frame(window,bg='#97CE68')
+frame = Frame(window,bg='#008000')
 
 #creating image 
 width = 500
@@ -127,17 +163,19 @@ canvas = Canvas(window, width=width, height=height)
 canvas.create_image(width/2,height/2,image=image)
 canvas.pack()
 #add text
-label_title = Label(frame,text="Enter your name",font=("Courrier",40), bg = "#97CE68",fg="black")
+label_title = Label(frame,text="Name ",font=("Courrier",40), bg = "#008000",fg="black")
 label_title.grid(row=1,column=0,sticky=W)
-name_entry = Entry(frame,font=("Courrier",40), bg = "#97CE68",fg="black")
+name_entry = Entry(frame,font=("Courrier",40), bg = "white",fg="black")
 name_entry.grid(row=1,column=1,sticky=W)
 print(name_entry.get()) 
 
 frame.pack(expand=YES)
 #addign a button 
+play_button = Button(window,text="Play",font=("Courrier",40), bg = "white",fg="green",command=rungame)
+play_button.pack(fill=X)
 play_button = Button(window,text="Tutorial",font=("Courrier",40), bg = "white",fg="green",command=tutorial)
 play_button.pack(fill=X)
-play_button = Button(window,text="Play",font=("Courrier",40), bg = "white",fg="green",command=rungame)
+play_button = Button(window,text="Credits",font=("Courrier",40), bg = "white",fg="green",command=credits)
 play_button.pack(fill=X)
 
 
@@ -167,7 +205,7 @@ if game.is_playing == True:
         if game.is_playing:
             game.player.update_animation()
             game.player.start_animation()
-
+            P1.damage(0.01)
              # Every game events
             for event in pygame.event.get():
                 if event.type == INC_SPEED:
@@ -221,7 +259,7 @@ if game.is_playing == True:
 
 
 
-
+            
             FramePerSec.tick(FPS)
 
         else:   
@@ -236,9 +274,9 @@ if game.is_playing == True:
             Police1 = pygame.font.Font("Fonts/Eczar-ExtraBold.ttf", 110)
             Police2 = pygame.font.Font("Fonts/Eczar-SemiBold.ttf", 50)
             Gameover = Police1.render("GAME OVER ", 0, (0,0,0)) 
-            YourScore = Police2.render("YOUR SCORE", 1,(255,100,100))
-            HighScore = Police2.render("HIGHSCORE" ,1,(255,100,100))
-            Score1 = Police2.render(str(game.score),1,(255,100,100))
+            YourScore = Police2.render("YOUR SCORE", 1,(255,0,0))
+            HighScore = Police2.render("BEST SCORE" ,1,(255,0,0))
+            Score1 = Police2.render(str(game.score),1,(255,50,50))
             if os.path.exists("scores.txt"):
                 if os.path.getsize("scores.txt") == 0:
                     file = open("scores.txt","w")
@@ -251,17 +289,22 @@ if game.is_playing == True:
                     line = file.readline()
                     tabline = line.split(' ')
                     file.close()
+                    name = tabline[0]
                     highest_score = tabline[1]
-                    Score2 = Police2.render(str(int(highest_score)),1,(255,100,100))
+                    name2 = Police2.render(name,1,(255,50,50))
+                    Score2 = Police2.render(str(int(highest_score)),1,(255,50,50))
                 else:
                     file = open("scores.txt",'r')
                     line = file.readline()
                     tabline = line.split(' ')
                     file.close()
+                    name = tabline[0]
                     highest_score = tabline[1]
-                    Score2 = Police2.render(str(int(highest_score)),1,(255,100,100))
+                    name2 = Police2.render(name,1,(255,50,50))
+                    Score2 = Police2.render(str(int(highest_score)),1,(255,50,50))
                     if game.score > int(highest_score):
-                        Score2 = Police2.render(str(game.score),1,(255,100,100))
+                        Score2 = Police2.render(str(game.score),1,(255,50,50))
+                        name2 = Police2.render(name,1,(255,50,50))
                         file = open("scores.txt","w")
                         file.write(game.player.name)
                         file.write(' ')
@@ -278,10 +321,9 @@ if game.is_playing == True:
                 file.close()
             DISPLAYSURF.blit(Gameover, (190, 50))
             DISPLAYSURF.blit(YourScore, (100,225))
-            DISPLAYSURF.blit(Score1, (200,300))
+            DISPLAYSURF.blit(Score1, (225,300))
             DISPLAYSURF.blit(HighScore,(625,225))
-            DISPLAYSURF.blit(Score2, (725,300))
-            pygame.display.update()     
-        pygame.display.update()   
-   
-        
+            DISPLAYSURF.blit(name2, (625,300))
+            DISPLAYSURF.blit(Score2, (625,365))
+            pygame.display.update()
+        pygame.display.update()
